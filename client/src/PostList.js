284089289243bbf069ada1177ca34922901798f9
evgeny <evgenyvolovlikov@gmail.com>
2024-10-06 20:57:04 +0300

    add event-bus & moderation services

--- client/src/PostList.js
+++ client/src/PostList.js
@@ -7,7 +7,8 @@ export const PostList = () => {
 	const [posts, setPosts] = useState({});
 
 	const fetchPosts = async () => {
-		const response = await axios.get('http://localhost:4000/posts');
+		const response = await axios.get('http://localhost:4002/posts');
+		console.log(response.data);
 		setPosts(response.data);
 	};
 
@@ -24,7 +25,7 @@ export const PostList = () => {
 			>
 				<div className="card-body">
 					<h3>{post.title}</h3>
-					<CommentList postId={post.id} />
+					<CommentList comments={post.comments} />
 					<CommentCreate postId={post.id} />
 				</div>
 			</div>
