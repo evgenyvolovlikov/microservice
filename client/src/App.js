6355e830f98a9467c7667335e37b2f0d05a6562f
evgeny <evgenyvolovlikov@gmail.com>
2024-10-06 15:19:00 +0300

    add post, comment services. Add client ui

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import { PostCreate } from './PostCreate';
+import { PostList } from './PostList';
+
+export const App = () => {
+	return (
+		<div className="container">
+			<h1>Create Post</h1>
+			<PostCreate />
+			<hr />
+			<PostList />
+		</div>
+	);
+};
